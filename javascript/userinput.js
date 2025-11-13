4e6f2e46765d492d55c3eadf457d49d04d481fc6
“Koysokkea9 <lkea29440@gmail.com>
2025-11-13 13:41:49 +0700

    justkidding

---
+++ javascript/userinput.js
@@ -0,0 +1,14 @@
+var studentid = prompt("1. Enter your student id");
+var studentname = prompt("2. Enter your student name");
+// var gender = prompt("3. Enter your gender");
+// var subject = prompt ("4. Enter your subject");
+// var payment = prompt("5. Enter yout payment");
+//1. output result on console html
+console.log("1. My personal id is:" +studentid);
+console.log("2. My personal name is:" +studentname);
+//output result on html form
+document.writeln("1. My personal id is:" +studentid +"<br>");
+document.writeln("1. My personal name is: " +studentname);
+// 3.output result on alert message
+alert("1.My personal id is: " +studentid + "\n" + "2.My personal name is: " +studentname); 
+// alert("1.My personal name is" +studentname);
